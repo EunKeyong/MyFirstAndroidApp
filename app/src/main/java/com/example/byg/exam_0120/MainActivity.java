eb0b5d37f29309c2994a816ff6aa0e156ea0395b
Eunkeyong.Bae <byke95@naver.com>
2017-01-25 11:48:41 +0900

    커피 앱 연습 완성

--- app/src/main/java/com/example/byg/exam_0120/MainActivity.java
+++ app/src/main/java/com/example/byg/exam_0120/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.byg.exam_0120;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,9 +11,15 @@ public class MainActivity extends AppCompatActivity {
 
     // 클래스 이름을 String으로 변환
     private static final String TAG = MainActivity.class.getSimpleName();
+    public static final int QUANTITY_MIN = 0;
+    public static final int QUANTITY_MAX = 10;
+    public static final int COFFEE_PRICE = 3000;
+
     private Button mMinusButton;
     private Button mPlusButton;
+    private Button mOrderButton;
     private TextView mQuantityTextView;
+    private TextView mResultTextView;
 
     // 수량을 나타내는 전역변수
     private int mQuantity;
@@ -22,7 +27,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // 초기화
         init();
 
@@ -32,32 +36,52 @@ public class MainActivity extends AppCompatActivity {
         // 레이아웃에서 특정 id를 인스턴스 변수와 연결
         mMinusButton = (Button) findViewById(R.id.minus_button);
         mPlusButton = (Button) findViewById(R.id.plus_button);
+        mOrderButton = (Button) findViewById(R.id.order_button);
         mQuantityTextView = (TextView) findViewById(R.id.quantity_text);
+        mResultTextView = (TextView) findViewById(R.id.result_text);
 
         // 무명 클래스
         mMinusButton.setOnClickListener(new View.OnClickListener() {
             // minusbutton 눌렀을때 확인
             @Override
             public void onClick(View view) {
-                // debug
-                Log.d(TAG, "마이너스 버튼 클릭");
-                // verbose
-                Log.v(TAG, "일반로그");
-                Log.e(TAG, "에러로그");
-                Log.i(TAG, "정보로그");
-                Log.w(TAG, "경고로그");
+                mQuantity--;
+                if (mQuantity < QUANTITY_MIN) {
+                    mQuantity = QUANTITY_MIN;
+                }
+                displayResult();
+            }
+        });
 
+        mPlusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mQuantity++;
+                if (mQuantity > QUANTITY_MAX) {
+                    mQuantity = QUANTITY_MAX;
+                }
+                displayResult();
+            }
+        });
 
-                // 토스트 메세지
-                Toast.makeText(MainActivity.this, "마이너스 버튼 클릭", Toast.LENGTH_SHORT).show();
-
-
+        mOrderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = mResultTextView.getText().toString();
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
             }
         });
     }
 
+    private void displayResult() {
+        mQuantityTextView.setText("" + mQuantity);
+//        int price = COFFEE_PRICE;
+        String result = "가격 : " + (COFFEE_PRICE * mQuantity) + "원\n감사합니다.";
+        mResultTextView.setText(result);
+    }
+
     // 변수초기화하는 메소드드
     private void init() {
-        mQuantity = 0;
+        mQuantity = QUANTITY_MIN;
     }
 }
