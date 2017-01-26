75d6d8c67c1923f3117fa35421a2369a7bda07bc
Eunkeyong.Bae <byke95@naver.com>
2017-01-26 10:55:32 +0900

    커피앱 - scrollview, checkbox연습

--- app/src/main/java/com/example/byg/exam_0120/MainActivity.java
+++ app/src/main/java/com/example/byg/exam_0120/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,15 +16,19 @@ public class MainActivity extends AppCompatActivity {
     public static final int QUANTITY_MIN = 0;
     public static final int QUANTITY_MAX = 10;
     public static final int COFFEE_PRICE = 3000;
+    public static final int CREAM_PRICE = 500;
 
     private Button mMinusButton;
     private Button mPlusButton;
     private Button mOrderButton;
     private TextView mQuantityTextView;
     private TextView mResultTextView;
+    private CheckBox mCreamCheckBox;
 
     // 수량을 나타내는 전역변수
     private int mQuantity;
+    // 휘핑크림
+    private boolean mIsCream;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         mOrderButton = (Button) findViewById(R.id.order_button);
         mQuantityTextView = (TextView) findViewById(R.id.quantity_text);
         mResultTextView = (TextView) findViewById(R.id.result_text);
+        mCreamCheckBox = (CheckBox) findViewById(R.id.cream_check);
 
         // 무명 클래스
         mMinusButton.setOnClickListener(new View.OnClickListener() {
@@ -69,6 +76,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String message = mResultTextView.getText().toString();
                 Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                init();
+            }
+        });
+
+        mCreamCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // 눌리면 true
+                mIsCream = isChecked;
+                displayResult();
             }
         });
     }
@@ -76,7 +93,13 @@ public class MainActivity extends AppCompatActivity {
     private void displayResult() {
         mQuantityTextView.setText("" + mQuantity);
 //        int price = COFFEE_PRICE;
-        String result = "가격 : " + (COFFEE_PRICE * mQuantity) + "원\n감사합니다.";
+        int total = COFFEE_PRICE * mQuantity;
+
+        if (mIsCream) {
+            total += CREAM_PRICE;
+        }
+        String result = String.format("가격 : %d원\n수량 : %d개\n휘핑크림 : %s\n감사합니다",
+                COFFEE_PRICE * mQuantity, mQuantity, mIsCream);
         mResultTextView.setText(result);
     }
 
