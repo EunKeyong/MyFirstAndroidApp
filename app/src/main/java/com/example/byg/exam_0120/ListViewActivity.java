5584fd721ce05ec4117b02b9988a3d6e541337a6
Eunkeyong.Bae <byke95@naver.com>
2017-02-06 11:17:29 +0900

    ListView, ArrayAdapter연습

---
+++ app/src/main/java/com/example/byg/exam_0120/ListViewActivity.java
@@ -0,0 +1,51 @@
+package com.example.byg.exam_0120;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class ListViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+
+        // 뷰
+        ListView listView = (ListView) findViewById(R.id.list_view);
+
+        // 데이터
+        ArrayList<String> data = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            data.add("이것은 데이타" + i);
+        }
+
+        // 어댑터
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, data);
+
+        listView.setAdapter(adapter);
+
+        // 클릭이벤트
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Toast.makeText(ListViewActivity.this, "그냥 클릭"+ position , Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Toast.makeText(ListViewActivity.this, "롱클릭"+ position, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+}
