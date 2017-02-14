e2f9388bbcca538d6435b2b7a4ed4cd4d8092dfe
Eunkeyong.Bae <byke95@naver.com>
2017-02-14 13:25:48 +0900

    Fragment : 액티비티에 프래그먼트 배치 및 사용 방법

--- app/src/main/java/com/example/byg/exam_0120/fragments/ColorFragment.java
+++ app/src/main/java/com/example/byg/exam_0120/fragments/ColorFragment.java
@@ -1,6 +1,6 @@
 package com.example.byg.exam_0120.fragments;
 
-import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -13,28 +13,20 @@ import com.example.byg.exam_0120.R;
 
 public class ColorFragment extends Fragment {
 
-    // 빈 생성자가 필요, 파라미터를 가질 수 없다
-    // (액티비티의 라이프사이클에 맞게 동작하게 하기 위해)
+    // 필수
     public ColorFragment() {
-        // Required empty public constructor
     }
 
-
+    @Nullable
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_color, container, false);
+        view.setBackgroundColor(Color.YELLOW);
+        return view;
     }
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void setColor(int color) {
+        getView().setBackgroundColor(color);
     }
 
-    // 액티비티의 onCreate()
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_color, container, false);
-    }
 }
