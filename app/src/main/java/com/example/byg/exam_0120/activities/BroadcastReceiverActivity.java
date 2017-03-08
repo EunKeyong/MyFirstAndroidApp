58f95b99db0b1c9bfe613c76c5212405978dcc8f
Eunkeyong.Bae <byke95@naver.com>
2017-03-08 09:25:13 +0900

    나만의 브로드캐스트 쏘기

--- app/src/main/java/com/example/byg/exam_0120/activities/BroadcastReceiverActivity.java
+++ app/src/main/java/com/example/byg/exam_0120/activities/BroadcastReceiverActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.byg.exam_0120.R;
 import com.example.byg.exam_0120.receiver.MyLocalReceiver;
@@ -14,7 +15,7 @@ import com.example.byg.exam_0120.receiver.MyLocalReceiver;
  * onStart(), onStop() 코드로 리시버 등록, 해제한다
  */
 
-public class BroadcastReceiverActivity extends AppCompatActivity {
+public class BroadcastReceiverActivity extends AppCompatActivity implements View.OnClickListener {
 
     private BroadcastReceiver mReceiver;
 
@@ -22,7 +23,7 @@ public class BroadcastReceiverActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_broadcast_reciver);
-
+        findViewById(R.id.button2).setOnClickListener(this);
 
     }
 
@@ -36,7 +37,7 @@ public class BroadcastReceiverActivity extends AppCompatActivity {
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_BATTERY_LOW);
         filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
-
+        filter.addAction("com.example.exam_0120.broadcast.ACTION_TEST");
         registerReceiver(mReceiver,filter);
     }
 
@@ -46,5 +47,19 @@ public class BroadcastReceiverActivity extends AppCompatActivity {
 
         // 리시버 해제
         unregisterReceiver(mReceiver);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        // 나만의 액션을 쏘기
+        Intent intent = new Intent("com.example.exam_0120.broadcast.ACTION_TEST");
+
+        // 순서 없는
+        sendBroadcast(intent);
+
+        // 순서 있는
+        //sendOrderedBroadcast();
+
     }
 }
--- app/src/main/java/com/example/byg/exam_0120/receiver/MyLocalReceiver.java
+++ app/src/main/java/com/example/byg/exam_0120/receiver/MyLocalReceiver.java
@@ -1,18 +1,74 @@
 package com.example.byg.exam_0120.receiver;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.widget.Toast;
+
+import com.example.byg.exam_0120.R;
+import com.example.byg.exam_0120.activities.BroadcastReceiverActivity;
 
 public class MyLocalReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-       if(intent.getAction().equals(Intent.ACTION_BATTERY_LOW)) {
-           Toast.makeText(context, "로컬 브로드캐스트 리시버다", Toast.LENGTH_SHORT).show();
-       } else if(intent.getAction().equals(Intent.ACTION_POWER_DISCONNECTED)) {
-           Toast.makeText(context, "전원이 뽑혔습니다", Toast.LENGTH_SHORT).show();
-       }
+        if (intent.getAction().equals(Intent.ACTION_BATTERY_LOW)) {
+            Toast.makeText(context, "로컬 브로드캐스트 리시버다", Toast.LENGTH_SHORT).show();
+
+        } else if (intent.getAction().equals(Intent.ACTION_POWER_DISCONNECTED)) {
+            Toast.makeText(context, "전원이 뽑혔습니다", Toast.LENGTH_SHORT).show();
+
+        } else if (intent.getAction().equals("com.example.exam_0120.broadcast.ACTION_TEST")) {
+            Toast.makeText(context, "나만의 액션받기 성공", Toast.LENGTH_SHORT).show();
+            showNotification(context);
+        }
+    }
+
+    private void showNotification(Context context) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        builder.setContentTitle("나만의 알림");
+        builder.setContentText("나만의 텍스트");
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher);
+        builder.setLargeIcon(bitmap);
+
+        // 알림을 클릭하면 수행될 인텐트
+        Intent resultIntent = new Intent(context, BroadcastReceiverActivity.class);
+
+        // 클릭하면 그액티비티로 돌아감
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, resultIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(pendingIntent);
+
+        // 클릭하면 알림 지우기
+        builder.setAutoCancel(true);
+
+        // 색상
+        builder.setColor(Color.YELLOW);
+
+        // 기본 알림음
+        Uri uri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        builder.setSound(uri);
+
+        // 진동
+        builder.setVibrate(new long[]{100, 200, 300});
+
+        // 액션
+        builder.addAction(R.mipmap.ic_launcher, "확인", pendingIntent);
+        builder.addAction(R.mipmap.ic_launcher, "취소", pendingIntent);
+
+        // 알림 표시
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(context);
+        managerCompat.notify(0, builder.build());
+
     }
 }
