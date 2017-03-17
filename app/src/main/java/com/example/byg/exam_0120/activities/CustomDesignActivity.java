eae242cf293e7d1d1708ffdf41af4e5a312e3a23
Eunkeyong.Bae <byke95@naver.com>
2017-03-17 11:27:28 +0900

    나만의 토스트 만들기 : shape Drawable 작성,  LiveTemplate mt 로 등록

---
+++ app/src/main/java/com/example/byg/exam_0120/activities/CustomDesignActivity.java
@@ -0,0 +1,15 @@
+package com.example.byg.exam_0120.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.byg.exam_0120.R;
+
+public class CustomDesignActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_design);
+    }
+}
